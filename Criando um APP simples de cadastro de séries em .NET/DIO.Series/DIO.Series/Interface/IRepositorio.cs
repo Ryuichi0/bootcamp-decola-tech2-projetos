951c0a911e7b786b6d5af77849b19936c29adf22
Ryuichi0 <thiagotnk11@gmail.com>
2021-12-06 08:05:46 -0300

    primeiro commit

---
+++ Criando um APP simples de cadastro de séries em .NET/DIO.Series/DIO.Series/Interface/IRepositorio.cs
@@ -0,0 +1,12 @@
+﻿namespace DIO.Series.Interface
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornaPorId(int id);
+        void Insere(T entidade);
+        void Exclui(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+    }
+}
